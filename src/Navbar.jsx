f10e8d7d36a9f6dad1ea6b113ce281823af76a55
Struth129 <sakets.ug18.ee@gmail.com>
2021-02-26 00:04:22 +0530

    a

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,7 +15,7 @@ const Example = (props) => {
       <div className="col-md-10 col-10 mx-auto">
       <nav className="navbar navbar-expand-lg navbar-light">
   <div className="container-fluid">
-    <a className="navbar-brand " disabled href="/">ReactJS</a>
+    <h3 className="navbar-brand " disabled href="/">ReactJS</h3>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
